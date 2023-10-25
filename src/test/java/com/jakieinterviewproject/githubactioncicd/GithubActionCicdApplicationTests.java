e5d0080a33a0e2f77e7e34138e05425c64292ce7
Jackie Singh <jakiesingh637@gmail.com>
2023-10-25 20:30:27 +0530

    first commit

---
+++ src/test/java/com/jakieinterviewproject/githubactioncicd/GithubActionCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakieinterviewproject.githubactioncicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
